47e5c7b740212368b395d95faf93cdbd32d47222
Peter Georgas <peter_georgas@yahoo.com>
2021-08-31 23:06:06 -0400

    Original code -- soon to be updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import marked from "marked";
+
+function getMarkdownText() {
+  var rawMarkup = marked(
+    "# This is markdown!\n ## This is markdown!\n ### _This is markdown_!\n`monkeys can code too!`",
+    {
+      sanitize: true,
+    }
+  );
+  return { __html: rawMarkup };
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="surround">
+        <div className="surroundtwo">
+          <div className="boxy border border-info">
+            <Container className="my-auto" id="textarea-holder">
+              <Row>
+                <Col md="6">
+                  <div className="markdown-box">
+                    <FloatingLabel controlId="editor" label="Markdown">
+                      <Form.Control
+                        as="textarea"
+                        placeholder="Enter your Markdown here"
+                        style={{ height: "500px" }}
+                      />
+                    </FloatingLabel>
+                  </div>
+                </Col>
+                <Col md="6">
+                  <div
+                    className="markdown-holder"
+                    id="preview"
+                    dangerouslySetInnerHTML={getMarkdownText()}
+                  ></div>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
